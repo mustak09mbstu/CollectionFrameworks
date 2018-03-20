d35771a2af8194539ed834ad978694773fbbc971
mustak09mbstu <37360197+mustak09mbstu@users.noreply.github.com>
2018-03-20 11:23:26 +0600

    commit

--- src/ConversionOfArrayToLinkedList.java
+++ src/ConversionOfArrayToLinkedList.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class ConversionOfArrayToLinkedList {
     public static void main(String[] args){
@@ -11,6 +9,46 @@ public class ConversionOfArrayToLinkedList {
         }
 
         System.out.print(ls);
+        ls.add(10);
+        System.out.println();
+        System.out.print(ls);
+
+        ls.add(0,20);
+
+        System.out.println();
+        System.out.print(ls);
+
+        Collections.sort(ls);
+
+        System.out.println();
+        System.out.print(ls);
+
+
+        class MyComparator implements Comparator {
+            public int compare(Object obj1,Object obj2){
+                Integer s1=(Integer) obj1;
+                Integer s2=(Integer) obj2;
+                return s2.compareTo(s1);
+            }
+        }
+        Collections.sort(ls,new MyComparator());
+        System.out.println();
+        System.out.print(ls);
+
+   String str[]={"mustafiz","Mahmud","Reza","Masud"};
+   LinkedList lst=new LinkedList(Arrays.asList(str));
+   System.out.println();
+   System.out.print(lst);
+
+   int[] A={586,547,528,145,159,369,489};
+   LinkedList<Integer> ld=new LinkedList<Integer>();
+   for (int i:A){
+       ld.add(i);
+   }
+        System.out.println();
+        System.out.print(ld+" ");
+
+
 
     }
 }
