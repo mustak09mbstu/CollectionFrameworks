d47ccda818ff82c89128564bd3e0eb5497434528
mustak09mbstu <37360197+mustak09mbstu@users.noreply.github.com>
2018-03-20 12:44:25 +0600

    Class Name Changed

--- src/ConversionOfArrayToArrayList.java
+++ src/ConversionOfArrayToArrayList.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ConversionOfArrayToList {
+public class ConversionOfArrayToArrayList {
     public static void main(String[] args) {
 
         /*Initializing object Type Array*/
